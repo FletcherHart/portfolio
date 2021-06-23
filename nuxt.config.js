441c6b1ab19d04b14098a6679204ec880950e63a
FletcherHart <fletcherhart@gmail.com>
2021-06-23 14:30:19 -0400

    fix: change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'crypto-design',
+    title: 'Fletcher Hart Portfolio',
     htmlAttrs: {
       lang: 'en',
     },
